f0bfa90f0d17f8b90904efd4e844c222cfd06874
Mihail Gorceag <mihailgorceag@Mihails-MacBook-Pro.local>
2021-05-12 19:11:30 +0300

    implement migrations

--- index.js
+++ index.js
@@ -22,7 +22,59 @@ const client = new Client({
 const main = async () => {
   await client.connect()
 
-  const res = await client.query('SELECT * FROM users')
+  client.query(`
+  CREATE TABLE Users (
+    id  SERIAL PRIMARY KEY,
+    nickname varchar(255),
+    email varchar(255),
+    created DATE default CURRENT_TIMESTAMP,
+    password varchar(255)
+  )
+  
+  CREATE TABLE Chat (
+    id SERIAL PRIMARY KEY,
+    created DATE default CURRENT_TIMESTAMP
+  )
+  
+  CREATE TABLE chat_member (
+      id SERIAL PRIMARY KEY,
+    
+      user_id integer, 
+      CONSTRAINT fk_user_id
+         FOREIGN KEY (user_id) 
+         REFERENCES users (id),
+    
+      chat_id integer, 
+      CONSTRAINT fk_chat_id
+         FOREIGN KEY (chat_id) 
+         REFERENCES chat (id)
+    )
+        
+  
+  CREATE TABLE Message (
+      id SERIAL PRIMARY KEY,
+      text TEXT,
+      created DATE default CURRENT_TIMESTAMP,
+    
+      user_id integer, 
+      constraint fk_user_id
+         foreign key (user_id) 
+         REFERENCES users (id),
+    
+      chat_id integer, 
+      constraint fk_chat_id
+         foreign key (chat_id) 
+         REFERENCES chat (id),
+  
+      chat_member_id integer,
+      constraint fk_chat_member_id
+      FOREIGN KEY (chat_member_id)
+      REFERENCES chat_member(id)
+    
+    )
+  
+  `)
+  // const res = await client.query('SELECT * FROM users')
 
 
   
